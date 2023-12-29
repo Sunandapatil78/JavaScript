728e2fbf8a78e140d0451f31c3c6ba46d495ee19
sunanda patil <sunandapatil13470@gmail.com>
2023-12-29 16:03:49 +0530

    Create 03_stringBasicAssign02 file
    Create 03_stringBasicAssign02 file

---
+++ 03_stringBasicAssign02.js
@@ -0,0 +1,17 @@
+function stringbasic() {
+    
+    var dreamCompany ="Microsoft";
+    console.log(`Dream Company Is : ${dreamCompany}`);
+    
+     var hobby1 =" Reading   ";
+     var hobby2 ="  Cooking  ";
+     var hobby3 = "  Coding  ";
+    
+    console.log(`My Hobbies are :${hobby1},${hobby2},${hobby3}`);
+
+         var result =( hobby1.length + hobby2.length+ hobby3 .length);
+        
+
+         console.log(`Sum Of The Total No is : ${result}`);
+}
+stringbasic();
---
+++ 03stringHandsOn.js
@@ -0,0 +1,60 @@
+
+
+function stringHandsOn(params) {
+var givenString ="  Hey you are doing good, keep it up  "
+console.log(` 1.Given String is : ${givenString}` )
+var result =(givenString.length)
+console.log(` 2.Length of string : ${result}`);
+
+result=  givenString . trim();
+console.log(` 3.String trim is :${result}`);
+console.log( ` After Trim :${result.length}`);
+
+var givenString = "Hey you are doing good, keep it up";
+var result = givenString.split(" ") .length;
+console.log(` 4.Total words Extra Space in string is: ${result}`);
+
+
+var givenString = "Hey you are doing good, keep it up";
+ 
+result = (givenString.charAt(0) )
+
+result1 =givenString .charAt(givenString .length -1)
+
+console.log(` 5.First Char is: ${result}  , Last Char is : ${result1}`);
+
+
+var givenString = "Hey you are doing good, keep it up";
+var result = givenString.split(" ") ,length;
+
+console.log(` 6.Total words in string is: ${result.length}`);
+
+var givenString = "Hey you are doing good, keep it up";
+var indexOfgood = givenString.indexOf('good');
+console.log(` 7.Index of  word "Good" is:  ${indexOfgood}` );
+
+
+var givenString = "Hey you are doing good, keep it up";
+
+  result = givenString.substring (22);
+
+  res = givenString.slice(22);
+   
+  console.log(` 8.Print the substring strating from index 22 :${result} ,Print the substring strating from index 22${res} `);
+
+  
+  var givenString = "  Hey you are doing good, keep it up  ";
+ 
+  result = givenString.endsWith ("up");
+  console.log(` 9.Print the substring End with word : ${result}`);
+
+
+
+  var givenString = "  Hey you are doing good, keep it up  ";
+ 
+  result = givenString.startsWith ("Hey");
+  console.log(` 10.Print the substring End with word : ${result}`);
+
+  
+}
+stringHandsOn()
